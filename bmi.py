e8492cec9dfffec4850e447b5b1af85c57012d1b
yasasvini <yasasvini.24@ichat.sp.edu.sg>
2025-05-04 11:40:40 +0800

    bmi test edit

--- bmi.py
+++ bmi.py
@@ -8,17 +8,20 @@ def calculate_bmi(height, weight):
 def classify_bmi(bmi):
     if bmi < 18.5:
         print("Underweight")
+        print("-1")
     elif 18.5 <= bmi < 25:
         print("Normal weight")
+        print("0")
     else:
         print("Overweight")
+        print("1")
     return
 
-def main():
-    bmiout = calculate_bmi(weight=57, height=1.73)
-    print(f"Your BMI is: {bmiout:.2f}")
-    classify_bmi(bmiout)
+#def main():
+#    bmiout = calculate_bmi(weight=60, height=1.50)
+#    print(f"Your BMI is: {bmiout:.2f}")
+#    classify_bmi(bmiout)
 
-bmiout=calculate_bmi(weight=57, height=1.73)
+bmiout=calculate_bmi(weight=60, height=1.5)
 print(f"Your BMI is: {bmiout:.2f}")
 classify_bmi(bmiout)
